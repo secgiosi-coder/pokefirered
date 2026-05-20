531f265f71af119d3ecacfa3060d0f6934a86f03
secgiosi-coder <sec.giosi@gmail.com>
2026-05-20 08:51:35 +0000

    Build 0.1 setup and mapping workflow

--- build_tools.sh
+++ build_tools.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-make -C tools/aif2pcm CXX=${1:-g++}
+code build_tools.sh#!/bin/sh
+# make -C tools/aif2pcm CXX=${1:-g++}
 make -C tools/bin2c CXX=${1:-g++}
 make -C tools/gbafix CXX=${1:-g++}
 make -C tools/gbagfx CXX=${1:-g++}
